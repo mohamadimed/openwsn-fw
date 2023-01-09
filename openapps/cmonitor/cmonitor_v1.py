f840f27d7b342810fd2ad10299ad479317148559
Mohamed <momomohamadi@yahoo.fr>
2023-01-09 15:18:56 +0100

    cmonitor_v1 generating and processing Tracks automatically

--- openapps/cmonitor/cmonitor_v1.py
+++ openapps/cmonitor/cmonitor_v1.py
@@ -15,6 +15,10 @@ class Cmonitor():
     DAGRank_List    = [256]
     mote_index_map  = [['d3','19']]
     matrix          = []
+    ingress         = 2
+    egress          = 0
+    NODES_IN_NETWORK = 2 #Important to set it correct, according  the number of nodes in the network
+    list_of_subTracks = []
 
     def __init__(self):
        return
@@ -36,7 +40,7 @@ class Cmonitor():
         #         if (i==j):
         #             matrix[i][j]= [999,999,999]
 
-        print self.matrix
+        #print self.matrix
         return self.matrix
 
     def update_matrix(self,source,rssi_value,neighbor,radio):#here we update the matrix with to value of rssi to which neighbor on which radio
@@ -55,10 +59,10 @@ class Cmonitor():
 
                         self.matrix[i][j] =[a,b,c]
                         self.matrix[i][j][radio] =rssi_value
-                        print self.matrix[i][j]
+                        #print self.matrix[i][j]
 
                         break
-        print self.matrix
+        #print self.matrix
 
         return 0
 
@@ -71,12 +75,58 @@ class Cmonitor():
                     return i
 
 
+    def get_mote_address_by_index(self,index):
+
+        for i in range(len(self.mote_index_map)):
+            if (index == i):
+                    return [self.mote_index_map[i][0],self.mote_index_map[i][1]]
+
+    def best_rssi_among_radios(self,rssi_per_radio):
+            best = rssi_per_radio [0]
+            index = 0
+            for i in range(1,len(rssi_per_radio)):
+                if rssi_per_radio[i] > best: #here we get the minimum (test is done with positif values)
+                    best = rssi_per_radio[i]
+                    index = i
+
+            return index #here we return the index of the best radio to be used
+
+
+    def create_track(self):
+                                                    #Need to define a way to set the used royte to zero/high number so it won't be chosen when calculating 'n' route
+        current_node = self.ingress
+
+
+        while (current_node != self.egress):
+                        
+            for i in  range(self.NODES_IN_NETWORK):
+                
+                if(self.matrix[current_node][i]!=[0,0,0]):
+                    #we have a neighbor
+
+                    #we should test if our DagRank is lower then it so we don't consider it as a neighbor (goes down)
+                    if (self.DAGRank_List[current_node] < self.DAGRank_List [i]):
+                        continue; #don't consider it as a neighbor
+
+                    else:
+                        node = i
+                        radio = self.best_rssi_among_radios(self.matrix[current_node][i])
+                        self.matrix[current_node][i][radio] = -999 # set value of link to 0 when select this link to be part of the track
+                        self.list_of_subTracks.append([self.get_mote_address_by_index(i),radio]) #next step of track [next node @, node radio] to beused for track creation
+                        print ("next step is node {} on radio {}".format(self.get_mote_address_by_index(i),radio))
+                        current_node = node #later i need to do mapping between node index and node address
+                        
+
+
+                else: continue; #we dont have a neighbor
+        return self.list_of_subTracks
+
 #---------------------------MAIN APP---------------------------#
 
 MOTE_IP = 'bbbb::12:4b00:14b5:d366'
 UDPPORT = 61618 # can't be the port used in OV
 
-NODES_IN_NETWORK = 2
+
 
 
 
@@ -97,7 +147,7 @@ print("\nWaiting for all nodes to be reachable  downward...")
 motes_list = netinfo.get_motes_list()
 
 #loop until get at least one reachable mote
-while motes_list == [] or len(motes_list) != NODES_IN_NETWORK:
+while motes_list == [] or len(motes_list) != (monitor.NODES_IN_NETWORK-1):
     del motes_list[:]
     motes_list = netinfo.get_motes_list()
 
@@ -112,7 +162,7 @@ for i in range(len(motes_list)):
     byte0 = motes_list[i][-4:-2]
     byte1 = motes_list[i][-2:]
     monitor.mote_index_map.append([byte0,byte1])
-print monitor.mote_index_map
+#print monitor.mote_index_map
 
 #-----------cerate and fill matrix-----------
 matrix = monitor.init_matrix(motes_list)
@@ -150,14 +200,14 @@ for i in range(1,len(monitor.mote_index_map)):
     MOTE_IP      = 'bbbb::12:4b00:14b5:'+monitor.mote_index_map[i][0]+monitor.mote_index_map[i][1]
     my_index     = monitor.get_mote_index_by_address(int(monitor.mote_index_map[i][0],16),int(monitor.mote_index_map[i][1],16))
     print "Asking mote: ",MOTE_IP, "for his neighborList"
-    print "my index is ", my_index
+    #print "my index is ", my_index
 
     c = coap.coap(udpPort=UDPPORT)
     p = c.GET('coap://[{0}]/m/nl'.format(MOTE_IP))
     c.close()
 
     neighborsCount = p[-1]
-    print "Number of neighbors is :",neighborsCount
+    #print "Number of neighbors is :",neighborsCount
 
     for i in range(0,len(p)-1,4):
         byte0 = p[i]
@@ -170,14 +220,38 @@ for i in range(1,len(monitor.mote_index_map)):
 
         #get the indext of the neighbor by his address
         neighbor_index = monitor.get_mote_index_by_address(byte0,byte1)
-        print "index of node is", neighbor_index
+        #print "index of node is", neighbor_index
 
         #update the matrix and set the RSSI values in the correct palce
         monitor.update_matrix(my_index,rssi,neighbor_index,radio)
 
     print neighborList
 
-print monitor.matrix
+
+print 'befor',monitor.matrix
+
+
+#---------------------CREATING generating the best TRACK----------------------------#
+print "\n\nWe have all the necessary materials to process the best Track.\n\
+please indicate the Ingress mote index among the following :"
+
+for i in range(1,len(monitor.mote_index_map)):
+
+    print i,  '= bbbb::12:4b00:14b5:'+monitor.mote_index_map[i][0]+monitor.mote_index_map[i][1] 
+
+choice = input("Select the INGRESS index\n")
+
+monitor.ingress=choice
+
+
+choice = input("Please indicate the number of Sub-Tracks to process\n")
+
+for i in range(choice):
+    monitor.create_track()
+
+for i in range(monitor.list_of_subTracks):
+
+    print 'TRACK',i,'  = ',monitor.list_of_subTracks
 # read the information about the board status
 #when only sending path0 ---> results is for now list of available resources paths
 # result: "</m/pr>,</m/nc>"
