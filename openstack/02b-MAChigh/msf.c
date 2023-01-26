bccf33225001c90b74a454b738c73837d0ba41ed
Mohamed <momomohamadi@yahoo.fr>
2023-01-26 13:49:10 +0100

    Shouldn't consider autonomous RX cells as Hard cells so it can be deleted on parent update

--- openstack/02b-MAChigh/msf.c
+++ openstack/02b-MAChigh/msf.c
@@ -64,7 +64,7 @@ void msf_init(void) {
         CELLRADIOSETTING_FALLBACK,                                       // default radio setting
         msf_hashFunction_getChanneloffset(idmanager_getMyID(ADDR_64B)),  // channel offset
         &temp_neighbor,                                                  //Neighbor
-        TRUE                                                             //Hard Cell? consider Autonoumous RX cell as hard cell (TRUE)                                        
+        FALSE                                                             //Hard Cell? consider Autonoumous RX cell as hard cell (TRUE)                                        
     );
 
     msf_vars.housekeepingTimerId = opentimers_create(TIMER_GENERAL_PURPOSE, TASKPRIO_MSF);
